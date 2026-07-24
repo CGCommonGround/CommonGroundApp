43b248188633463a0153eb11bb89b2086029ba2b
Mio <gusw.0246.lpsyg@gmail.com>
2026-07-24 13:44:34 +0200

    feat(cors)installed

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 require('./config/db'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/users', require('./routes/userRoutes'));
